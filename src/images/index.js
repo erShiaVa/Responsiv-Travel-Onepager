7e456a02d54c423c1f2de3c3625ffe0cdbb896b9
erShiaVa <vakili.ershia@gmail.com>
2023-07-24 17:47:19 +0200

    new images

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,8 @@
 export const images = {
     hero_img: require("../images/hero-img.jpg"),
+    iceland: require("../images/iceland.jpg"),
+    iceland_two: require("../images/iceland-two.jpg"),
+    iceland_three: require("../images/iceland-three.jpg"),
+    iceland_four: require("../images/iceland-four.jpg"),
+    iceland_five: require("../images/iceland-five.jpg"),
 };
